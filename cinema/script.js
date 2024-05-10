62be77ccb918b838cadfbe88a8b7146d02d5a390
mtcporto <mtcporto@gmail.com>
2024-05-10 00:26:06 -0300

    Update script.js

--- cinema/script.js
+++ cinema/script.js
@@ -1,41 +1,178 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const url = 'https://cors.mosaicoworkers.workers.dev/api-content.ingresso.com/v0/events/city/32/partnership/marcotulio';
+// Função para criar o elemento do filme
+function createMovieElement(movieId, movieData) {
+  const movieElement = document.createElement('div');
+  movieElement.classList.add('card', 'top-level');
+  
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Filtrando os eventos que não são do Festival Varilux
-            const nonVariluxEvents = data.items.filter(event => !event.tags.includes('Festival Varilux 2023'));
-            // Chamando a função para preencher a página com os eventos
-            populatePage(nonVariluxEvents);
-        })
-        .catch(error => console.error('Erro ao obter dados da API:', error));
-});
+  // Adicione a imagem do filme, se disponível
+  if (movieData.images && movieData.images.length > 0) {
+    const movieImage = document.createElement('img');
+    movieImage.src = movieData.images[0].url;
+    movieImage.alt = movieData.title;
+    movieImage.style.width = '350px'; // Define a largura da imagem
+    movieImage.classList.add('card-img-top'); // Adiciona a classe do Bootstrap
+    movieElement.appendChild(movieImage);
+  }
 
-function populatePage(data) {
-    // Obtendo a referência do contêiner de eventos
-    var eventContainer = document.getElementById('eventContainer');
-    // Loop através dos eventos
-    data.forEach(event => {
-        // Criando um elemento de div para cada evento
-        var eventDiv = document.createElement('div');
-        eventDiv.classList.add('event-container');
-        // Adicionando a imagem do evento (se houver)
-        if (event.images && event.images.length > 0) {
-            var imageElement = document.createElement('img');
-            imageElement.src = event.images[0].url; // Usando a primeira imagem como exemplo
-            imageElement.alt = event.title + ' Poster';
-            imageElement.classList.add('event-image');
-            imageElement.addEventListener('click', function () {
-                window.open(event.siteURL, '_blank');
-            });
-            eventDiv.appendChild(imageElement);
-        }
-        var titleElement = document.createElement('h2');
-        titleElement.classList.add('event-title');
-        titleElement.textContent = event.title;
-        eventDiv.appendChild(titleElement);
-        // Adicionando o evento ao contêiner principal
-        eventContainer.appendChild(eventDiv);
+// Cria o corpo do card
+const cardBody = document.createElement('div');
+cardBody.classList.add('card-body');
+movieElement.appendChild(cardBody);
+
+  // Detalhes do filme
+  const movieTitle = document.createElement('h5');
+  movieTitle.textContent = movieData.title;
+  movieTitle.classList.add('card-title'); // Adiciona a classe do Bootstrap
+  cardBody.appendChild(movieTitle);
+
+  // Adicione detalhes adicionais do filme
+  const movieDetails = document.createElement('p');
+  movieDetails.textContent = ``;
+  movieDetails.classList.add('card-text'); // Adiciona a classe do Bootstrap
+  cardBody.appendChild(movieDetails);
+
+
+  function getBadgeColor(rating) {
+    switch (rating) {
+      case 'Livre':
+        return 'badge-success';
+      case '10 anos':
+        return 'badge-primary';        
+      case '12 anos':
+        return 'badge-warning';
+      case '14 anos':
+        return 'badge-orange';
+      case '16 anos':
+        return 'badge-danger';
+      case '18 anos':
+        return 'badge-dark';
+      default:
+        return 'badge-secondary'; // Cor padrão para classificações desconhecidas
+    }
+  }
+  
+
+// Adicione a classificação como um badge
+const movieRating = document.createElement('span');
+movieRating.textContent = movieData.contentRating;
+movieRating.classList.add('badge', getBadgeColor(movieData.contentRating)); // Adiciona as classes do Bootstrap
+movieDetails.appendChild(movieRating);
+
+movieDetails.appendChild(document.createTextNode(` ${movieData.genres.join(', ')} - ${movieData.duration} min.`));
+
+// Cria o acordeão para as sessões
+const accordion = document.createElement('div');
+accordion.classList.add('accordion');
+accordion.id = `accordion${movieId}`;
+cardBody.appendChild(accordion);
+
+// Loop através dos cinemas e exibir detalhes
+let cinemaId = 0;
+for (let cinema in movieData.cinemas) {
+  const cinemaCard = document.createElement('div');
+  cinemaCard.classList.add('card');
+  accordion.appendChild(cinemaCard);
+
+
+    const cinemaHeader = document.createElement('div');
+    cinemaHeader.classList.add('card-header');
+    cinemaHeader.id = `heading${movieId}${cinemaId}`;
+    cinemaCard.appendChild(cinemaHeader);
+
+    const cinemaButton = document.createElement('button');
+    cinemaButton.classList.add('btn', 'btn-link');
+    cinemaButton.type = 'button';
+    cinemaButton.dataset.toggle = 'collapse';
+    cinemaButton.dataset.target = `#collapse${movieId}${cinemaId}`;
+    cinemaButton.textContent = cinema;
+    cinemaHeader.appendChild(cinemaButton);
+
+    const cinemaCollapse = document.createElement('div');
+    cinemaCollapse.id = `collapse${movieId}${cinemaId}`;
+    cinemaCollapse.classList.add('collapse');
+    cinemaCollapse.dataset.parent = `#accordion${movieId}`;
+    cinemaCard.appendChild(cinemaCollapse);
+
+    const cinemaBody = document.createElement('div');
+    cinemaBody.classList.add('card-body');
+    cinemaCollapse.appendChild(cinemaBody);
+
+    movieData.cinemas[cinema].forEach(room => {
+      const roomName = document.createElement('h5');
+      roomName.textContent = room.name;
+      cinemaBody.appendChild(roomName);
+
+      room.sessions.forEach(session => {
+        const sessionDetails = document.createElement('p');
+        sessionDetails.classList.add('session-details');
+        sessionDetails.textContent = `${session.date} - ${session.time} - ${session.type}`;
+        cinemaBody.appendChild(sessionDetails);
+      });
     });
+
+    cinemaId++;
+  }
+
+  return movieElement;
 }
+
+const cityId = 32; // Substitua pelo ID da cidade real
+const urlCinemas = `https://cors.mosaicoworkers.workers.dev/api-content.ingresso.com/v0/theaters/city/${cityId}?partnership=marcotulio`;
+
+// Objeto para armazenar os filmes e suas sessões
+let movies = {};
+
+fetch(urlCinemas)
+  .then(response => response.json())
+  .then(data => {
+    if (data) {
+      let promises = data.map(cinema => {
+        const theaterId = cinema.id;
+        const urlSessions = `https://cors.mosaicoworkers.workers.dev/api-content.ingresso.com/v0/sessions/city/${cityId}/theater/${theaterId}?partnership=marcotulio`;
+        return fetch(urlSessions)
+          .then(response => response.json())
+          .then(sessionData => {
+            if (sessionData && sessionData[0] && sessionData[0].movies) {
+              sessionData[0].movies.forEach(movie => {
+                // Se o filme ainda não está no objeto movies, adicione-o
+                if (!movies[movie.id]) {
+                  movies[movie.id] = {
+                    title: movie.title,
+                    images: movie.images, // Adicione esta linha
+                    duration: movie.duration,
+                    contentRating: movie.contentRating,
+                    genres: movie.genres,
+                    cinemas: {}
+                  };
+                }
+                // Adicione o cinema e as sessões ao filme
+                movies[movie.id].cinemas[cinema.name] = movie.rooms.map(room => ({
+                  name: room.name,
+                  sessions: room.sessions.map(session => ({
+                    date: session.date.dayAndMonth,
+                    time: session.time,
+                    type: session.types[0].name,
+                  }))
+                }));
+              });
+            } else {
+              console.error('Dados de sessão não encontrados');
+            }
+          })
+          .catch(error => console.error('Erro ao obter dados da API:', error));
+      });
+
+      // Aguarde todas as promessas serem resolvidas
+      Promise.all(promises).then(() => {
+        const sessionsContainer = document.querySelector('.sessions-container');
+        for (let movieId in movies) {
+          const movieElement = createMovieElement(movieId, movies[movieId]);
+          sessionsContainer.appendChild(movieElement);
+        }
+      });
+    } else {
+      console.error('Dados de cinema não encontrados');
+    }
+  })
+  .catch(error => console.error('Erro ao obter dados da API:', error));
+ 
