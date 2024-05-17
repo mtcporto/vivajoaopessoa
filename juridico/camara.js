df867c4ada91ef93eabc3c24cdd676ed8779cf9e
mtcporto <mtcporto@gmail.com>
2024-05-16 22:14:59 -0300

    Create camara.js

---
+++ juridico/camara.js
@@ -0,0 +1,70 @@
+// camara.js
+function loadCamaraNews() {
+    const url = 'https://www.camara.leg.br/noticias/rss/dinamico/DIREITO-E-JUSTICA';
+    let newsCount = 0;
+    
+    function loadNews() {
+      fetch(url)
+        .then(response => response.text())
+        .then(data => {
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(data, 'text/xml');
+          const items = xmlDoc.querySelectorAll('item');
+    
+          items.forEach((item, index) => {
+            if (index >= newsCount && index < newsCount + 30) {
+              const title = item.querySelector('title').textContent;
+              const link = item.querySelector('link').textContent;
+              const pubDate = new Date(item.querySelector('pubDate').textContent).toLocaleString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+              });
+    
+              let imgURL = "https://via.placeholder.com/300x200.png?text=Sem+Imagem";
+              let contentEncoded = item.querySelector('content\\:encoded, encoded').textContent;
+              const parser = new DOMParser();
+              const contentDoc = parser.parseFromString(contentEncoded, 'text/html');
+              const imgElement = contentDoc.querySelector('img');
+              if (imgElement) {
+                imgURL = imgElement.getAttribute('src');
+              }
+    
+              const html = `
+                <div class="col-md-4">
+                  <div class="card mb-4 shadow-sm">
+                    <img src="${imgURL}" class="bd-placeholder-img card-img-top" width="100%" height="225">
+                    <div class="card-body">
+                    <p class="fonte">
+                    <span class="badge badge-danger">Fonte: Câmara dos Deputados</span>
+                    </p>                      
+                    <h5 class="card-title">${title}</h5>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <a href="${link}" target="_blank" class="btn btn-sm btn-outline-secondary">Ver Notícia</a>
+                        </div>
+                        <small class="text-muted">${pubDate}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              `;
+              document.querySelector("#newsCards").innerHTML += html;
+            }
+          });
+          newsCount += 30;
+          if (newsCount < items.length) {
+            document.querySelector("#loadMore").style.display = 'block';
+          } else {
+            document.querySelector("#loadMore").style.display = 'none';
+          }
+        })
+        .catch(error => console.error('Erro:', error));
+    }
+    
+    document.querySelector("#loadMore").addEventListener('click', loadNews);
+    loadNews();  
+}
